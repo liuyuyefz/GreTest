cf629af64f73d48cb9bbbf812a26f422d5752560
liuyuyefz <liuyuyefz@gmail.com>
2013-05-17 04:18:14 +0800

    规范了代码
    看pSettubgScene,pSettubg.plist,pSettubg.xml三个类

---
+++ GreTest/Classes/TableBar.cpp
@@ -0,0 +1,9 @@
+//
+//  TableBar.cpp
+//  GreTest
+//
+//  Created by lyy on 13-5-16.
+//
+//
+
+#include "TableBar.h"
---
+++ GreTest/Classes/TableBar.h
@@ -0,0 +1,14 @@
+//
+//  TableBar.h
+//  GreTest
+//
+//  Created by lyy on 13-5-16.
+//
+//
+
+#ifndef __GreTest__TableBar__
+#define __GreTest__TableBar__
+
+#include <iostream>
+
+#endif /* defined(__GreTest__TableBar__) */
